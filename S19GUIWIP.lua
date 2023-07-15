a045f90d2c0829105e8981d47454795ba17dfc1d
potatoei <108433529+potatoei@users.noreply.github.com>
2023-07-15 11:01:50 -0500

    Update S19GUIWIP.lua

--- S19GUIWIP.lua
+++ S19GUIWIP.lua
@@ -1,192 +1,156 @@
---[[
-        Thank you IY Devs for giving me the notify function, 
-        Thank you xHeptic for giving me the GUI
-        All other code is made by me or ChatGPT, thank you!
-        I will be working on this for the next few days.
-]]
--- preload
+if not game.PlaceId == 1440936008 and ALPHA_LOADED and not _G.GUILoaded then
+    return
+end
 
-local p = workspace.Ignore.Pickups
+pcall(function() getgenv().ALPHA_LOADED = true end)
 
--- pickup vars
-local Riot
-local GlockPickup
-local P90
-local KrissVector
-local Brush
-local med
-local Radio
-local Vest
-local Knives
-local Keychains
-local CFrame = speaker.CFrame
-local speaker = Players.LocalPlayer
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/forumsLib/main/source.lua"))()
-local Library = Library.new("Site-19 GUI Made by Jaystation/Opulent")
 local main = Library:NewSection("Loadouts")
-local main1 = Library:NewSection("SWAT Positions")
+local main1 = Library:NewSection("SWAT ig")
 local subs = Library:NewSection("Vision, Misc.")
-
-
-----------------------------------------------------------------------------------------
-
-shade1 = {}
-shade2 = {}
-shade3 = {}
-text1 = {}
-text2 = {}
-shade3 = {}
-text1 = {}
-text2 = {}
-
--- functions
-
-
-local function clickMiddleScreen() -- ChatGPT function
-    -- Get the UserInputService and Mouse objects
+local speaker = game.Players.LocalPlayer
+local picks = workspace.Ignore.Pickups
+function clickMiddleScreen()
     local userInputService = game:GetService("UserInputService")
     local mouse = game.Players.LocalPlayer:GetMouse()
-
-    -- Calculate the middle of the screen
     local screenWidth = game:GetService("Workspace").CurrentCamera.ViewportSize.X
     local screenHeight = game:GetService("Workspace").CurrentCamera.ViewportSize.Y
     local middlePosition = Vector2.new(screenWidth / 2, screenHeight / 2)
-
-    -- Trigger a mouse click at the middle position
     userInputService.MouseBehavior = Enum.MouseBehavior.Default
     mouse.X = middlePosition.X
     mouse.Y = middlePosition.Y
     userInputService:ClickMouse()
-
-    -- Restore the mouse position and behavior
     userInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
     mouse.X = screenWidth / 2
     mouse.Y = screenHeight / 2
 end
 
-
-function randomString() -- IY Function
-	local length = math.random(10,20)
-	local array = {}
-	for i = 1, length do
-		array[i] = string.char(math.random(32, 126))
-	end
-	return table.concat(array)
-end
--- IY Notify function
-pinNotification = nil
-local notifyCount = 0
-function notify(text,text2,length)
-	task.spawn(function()
-		local LnotifyCount = notifyCount+1
-		local notificationPinned = false
-		notifyCount = notifyCount+1
-		if pinNotification then pinNotification:Disconnect() end
-		pinNotification = PinButton.MouseButton1Click:Connect(function()
-			task.spawn(function()
-				pinNotification:Disconnect()
-				notificationPinned = true
-				Title_2.BackgroundTransparency = 1
-				wait(0.5)
-				Title_2.BackgroundTransparency = 0
-			end)
-		end)
-		Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, 0), "InOut", "Quart", 0.5, true, nil)
-		wait(0.6)
-		local closepressed = false
-		if text2 then
-			Title_2.Text = text
-			Text_2.Text = text2
-		else
-			Title_2.Text = 'Notification'
-			Text_2.Text = text
-		end
-		Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, -100), "InOut", "Quart", 0.5, true, nil)
-		CloseButton.MouseButton1Click:Connect(function()
-			Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, 0), "InOut", "Quart", 0.5, true, nil)
-			closepressed = true
-			pinNotification:Disconnect()
-		end)
-		if length and isNumber(length) then
-			wait(length)
-		else
-			wait(10)
-		end
-		if LnotifyCount == notifyCount then
-			if closepressed == false and notificationPinned == false then
-				pinNotification:Disconnect()
-				Notification:TweenPosition(UDim2.new(1, Notification.Position.X.Offset, 1, 0), "InOut", "Quart", 0.5, true, nil)
-			end
-			notifyCount = 0
-		end
-	end)
-end
--- Get Players
-Players = game:GetService("Players")
--- vars
-
-
-main:Seperator()
-
 main1:NewButton("SD SWAT Position", function()
-    for i, v in pairs(speaker:FindFirstChildOfClass("Backpack"):GetChildren()) do
-        if v:IsA("Tool") and v.Name == "P90" and v.Name == "Riot Shield" then
-            v.Parent = speaker.Character
-        else
-            notify("You don't have the P90 and/or Riot Shield!")
-        end
+    local riotShield = speaker:FindFirstChild("Riot Shield")
+    local p90 = speaker:FindFirstChild("P90")
+    
+    if riotShield and p90 then
+        riotShield.Parent = speaker.Character
+        p90.Parent = speaker.Character
+    else
+        notify("You don't have the Riot Shield and/or P90!")
     end
 end)
 
 main:NewButton("MTF Loadout", function()
     local krissVector = workspace.Ignore.Pickups:FindFirstChild("Kriss Vector")
+    local glock22 = workspace.Ignore.Pickups:FindFirstChild("Glock22")
+    local level3Card = workspace.Ignore.Pickups:FindFirstChild("Level-3 Card")
+    
     if krissVector and krissVector:IsA("Folder") then
         for _, part in ipairs(krissVector:GetDescendants()) do
             if part:IsA("Part") and part.Config and part.Config:FindFirstChild("Alive") and part.Config.Alive.Value then
                 local pickupPart = part:FindFirstChild("Pickup")
                 if pickupPart and pickupPart:IsA("Part") then
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pickupPart.CFrame
-                    break -- Stop searching after finding the first valid pickup
+                    task.wait()
+                    clickMiddleScreen()
+                    break
                 end
             end
         end
     end
-    task.wait(.2)
-
-
- 
-    local glockPickup = workspace.Ignore.Pickups:FindFirstChild("Glock22")
-    if glockPickup and glockPickup:IsA("Part") and glockPickup.Config and glockPickup.Config:FindFirstChild("Alive") and glockPickup.Config.Alive.Value then
-        local pickupPart = glockPickup:FindFirstChild("Pickup")
-        if pickupPart and pickupPart:IsA("Part") then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pickupPart.CFrame
-        end
-    end
-
     
-    local cardPickup = workspace.Ignore.Pickups:FindFirstChild("Keycards")
-    if cardPickup and cardPickup:IsA()
-
-
-    task.wait(.2)
-
-    local Riot = workspace.Ignore.Pickups:FindFirstChild("Riot Shield")
-    if Riot and Riot:IsA("Folder") then
-        for _, part in ipairs(Riot:GetDescendants()) do
-            if part:IsA("Part") and part.Config and part.Config:FindFirstChild("Alive") and part.Config.Alive.Value then
-                local pickupRiot = part:FindFirstChild("Pickup")
-                if pickupRiot and pickupRiot:IsA("Part") then
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pickupRiot.CFrame
-                    break -- Stop searching after finding the first valid pickup
-                end
-            end
+    if glock22 and glock22:IsA("Part") and glock22.Config and glock22.Config:FindFirstChild("Alive") and glock22.Config.Alive.Value then
+        local glock22PickupPart = glock22:FindFirstChild("Pickup")
+        if glock22PickupPart and glock22PickupPart:IsA("Part") then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = glock22PickupPart.CFrame
+            task.wait()
+            clickMiddleScreen()
+        end
+    end
+    
+    if level3Card and level3Card:IsA("Part") and level3Card.Config and level3Card.Config:FindFirstChild("Alive") and level3Card.Config.Alive.Value then
+        local level3CardPickupPart = level3Card:FindFirstChild("Pickup")
+        if level3CardPickupPart and level3CardPickupPart:IsA("Part") then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = level3CardPickupPart.CFrame
+            task.wait()
+            clickMiddleScreen()
         end
     end
 end)
 
 main:NewButton("SD Loadout w/ Shield", function()
+    local P90 = picks:FindFirstChild("P90")
+    local R = picks:FindFirstChild("Riot Shield")
+    local Rad = picks:FindFirstChild("Radio")
+    
+    for i, v in pairs(GetDescendants(P90)) do
+        if v.Name == "Pickup" then
+            local P90PickupPart = v
+            if P90PickupPart and P90PickupPart:IsA("Part") then
+                local config = P90PickupPart:FindFirstChild("Config")
+                if config and config:FindFirstChild("Alive") and config.Alive.Value == true then
+                    speaker.Character.HumanoidRootPart.CFrame = P90PickupPart.CFrame
+                    task.wait()
+                    clickMiddleScreen()
+                end
+            end
+        end
+    end
 
+    for i, v in pairs(GetDescendants(R)) do
+        if v.Name == "Pickup" and v:IsA("Part") then
+            local findr = v
+            local config2 = findr:FindFirstChild("Config")
+            if config2 and config2:FindFirstChild("Alive") and config.Alive.Value == true then
+                speaker.Character.HumanoidRootPart.CFrame = findr.CFrame
+                task.wait()
+                clickMiddleScreen()
+            end
+        end
+    end    
+
+    for i, v in pairs(GetDescendants(Rad)) do
+        if v.Name == "Pickup" then
+            local rapick = v 
+            if rapick and rapick:IsA("Part") then
+                local config3 = rapick:FindFirstChild("Config")
+                if config3 and config3:FindFirstChild("Alive") and config.Alive.Value == true then
+                    speaker.Character.HumanoidRootPart.CFrame = rapick.CFrame
+                    task.wait()
+                    clickMiddleScreen()
+                end
+            end
+        end
+    end
 end)
-subs:NewButton("Night Vision", function()
 
+
+-- nightvision status toggle
+local toggle = subs:NewToggle("Night Vision: ", function(status)
+    local nightVisionEnabled = status
+    local lighting = game:GetService("Lighting")
+    local nightVision = lighting.NightVision
+    nightVision.Enabled = nightVisionEnabled
+end)
+
+coroutine.wrap(function()
+    while wait() do
+        toggle:Update("Night Vision: "..tostring(nightVisionEnabled))
+    end
+end)()
+
+local userInputService = game:GetService("UserInputService")
+
+local function toggleNightVision()
+    nightVisionEnabled = not nightVisionEnabled
+    local lighting = game:GetService("Lighting")
+    local nightVision = lighting.NightVision
+    nightVision.Enabled = nightVisionEnabled
+end
+
+-- Check if the player is typing before toggling night vision
+userInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+    if not gameProcessedEvent and input.KeyCode == Enum.KeyCode.N then
+        if not userInputService:GetFocusedTextBox() then
+            toggleNightVision()
+        end
+    end
 end)
