10a6bc6027dcf543f538b668da366276dae6b649
potatoei <108433529+potatoei@users.noreply.github.com>
2023-08-09 17:08:27 -0500

    Secret

--- iyelect.lua
+++ iyelect.lua
@@ -232,7 +232,7 @@ Title.ZIndex = 10
 table.insert(shade1,Title)
 table.insert(text1,Title)
 
-local Titles = {"...", "Infinite uwus", "mod admin (real)", "rwaa", "Where am I", "America", "walter black", "n", "pwa", "not april fools"}
+local Titles = {"for i = 5.93, length do", "Infinite uwus", "mod admin (real)", "rwaa", "Where am I", "pwa"}
 local Images = {"11688231017", "11339922083", "11914981726", "5783845592", "6762179979", "7846015915", "11171958623", "11308149650", "7444547410", "6093472109"}
 
 Title.Text = Titles[math.random(1, #Titles)] .. " v" .. currentVersion
