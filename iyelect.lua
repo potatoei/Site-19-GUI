011b842286259c636acbd888c6da9249f4af7acb
potatoei <108433529+potatoei@users.noreply.github.com>
2024-08-20 18:50:34 -0500

    Update iyelect.lua
    REMOVED THE TITLES, ONLY INFINITE YIELD V

--- iyelect.lua
+++ iyelect.lua
@@ -240,7 +240,7 @@ Title.ZIndex = 10
 table.insert(shade1,Title)
 table.insert(text1,Title)
 
-local Titles = {"...", "Infinite Yield FE", "infinit","limited yield", "moderator admin", "admin admin", "admin panel", "Game Moderator Panel", "I love infinite yield", "KeepInfYield"}
+local Titles = {"Infinite Yield FE v"}
 local Images = {"11688231017", "11339922083", "11914981726", "5783845592", "6762179979", "7846015915", "11171958623", "11308149650", "7444547410", "6093472109"}
 
 Title.Text = Titles[math.random(1, #Titles)] .. " v" .. currentVersion
